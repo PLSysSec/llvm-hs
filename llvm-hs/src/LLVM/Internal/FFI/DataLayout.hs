c1c6084b43a2227184980dace8f684102fbfabd5
mlfbrown <mlfbrown@stanford.edu>
2019-04-30 12:50:58 -0700

    offset ffi

--- llvm-hs/src/LLVM/Internal/FFI/DataLayout.hs
+++ llvm-hs/src/LLVM/Internal/FFI/DataLayout.hs
@@ -1,26 +1,24 @@
-{-# LANGUAGE
-  ForeignFunctionInterface
-  #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 
 module LLVM.Internal.FFI.DataLayout where
 
-import LLVM.Prelude
+import           LLVM.Prelude
 
-import Foreign.C.String
-import Foreign.Ptr
+import           Foreign.C
+import           Foreign.Ptr
 
-import LLVM.Internal.FFI.LLVMCTypes
-import LLVM.Internal.FFI.PtrHierarchy
+import           LLVM.Internal.FFI.LLVMCTypes
+import           LLVM.Internal.FFI.PtrHierarchy
 
 data DataLayout
 
 -- Oooh those wacky LLVM C-API coders: C API called DataLayout TargetData.
 -- Great. Just great.
 
-foreign import ccall unsafe "LLVMCreateTargetData" createDataLayout :: 
+foreign import ccall unsafe "LLVMCreateTargetData" createDataLayout ::
   CString -> IO (Ptr DataLayout)
 
-foreign import ccall unsafe "LLVMDisposeTargetData" disposeDataLayout :: 
+foreign import ccall unsafe "LLVMDisposeTargetData" disposeDataLayout ::
   Ptr DataLayout -> IO ()
 
 foreign import ccall unsafe "LLVMCopyStringRepOfTargetData" dataLayoutToString ::
@@ -28,3 +26,6 @@ foreign import ccall unsafe "LLVMCopyStringRepOfTargetData" dataLayoutToString :
 
 foreign import ccall unsafe "LLVMABISizeOfType" getTypeAllocSize ::
   Ptr DataLayout -> Ptr Type -> IO Word64
+
+foreign import ccall unsafe "LLVMOffsetOfElement" getOffsetOfElement ::
+  Ptr DataLayout -> Ptr Type -> CUInt -> IO Word64
